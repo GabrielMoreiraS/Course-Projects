6e5b66c913acfd5b4c5b6f417779f3cd79ff2fdc
GabrielMS <73364268+GabrielMoreiraS@users.noreply.github.com>
2021-02-11 21:31:51 -0300

    Course-Projects.v.0.4

---
+++ Projeto_05/js/pictures.js
@@ -0,0 +1,31 @@
+const displayer = document.querySelector('[data-displayer]');
+const row = document.querySelector('[data-row]');
+const arrows = document.querySelectorAll('[data-arrow]');
+const pictures = document.querySelectorAll('[data-pic]');
+
+function changeDisplayer(){
+    pictures.forEach(e => {
+        e.onclick = () =>{
+            displayer.setAttribute('style',e.getAttribute('style'))
+        }
+    })
+}
+changeDisplayer();
+
+function changeOptions(){
+    arrows.forEach(e => {
+        var arrow = document.getElementById(e.getAttribute('id'));
+        arrow.addEventListener('click', ()=>{
+            var rowPos = row.offsetWidth;
+            if(arrow.getAttribute('id') == 'r'){
+                row.scrollBy(rowPos,0);
+            }else{
+                row.scrollBy(-rowPos,0);
+            }
+        })
+    })
+    window.onresize = () =>{
+        row.scrollTo(0,0);
+    }
+}
+changeOptions();
